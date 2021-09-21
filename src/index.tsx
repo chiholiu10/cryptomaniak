a3640efe4b0eb7651ff61c840a60165ecf972be5
Chiho Liu <chiho@succcessfactory.com>
2021-09-21 18:54:49 +0200

    basic setup

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { ThemeProvider } from "styled-components";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./styles/Theme";
+import { CSSreset } from "./styles/CssReset";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CSSreset />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
